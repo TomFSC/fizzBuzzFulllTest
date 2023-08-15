d716acc11d0dfc0e955ca312d06fc4a0a747c04e
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-15 15:13:16 +0200

    ♻️ use variables

--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -2,26 +2,33 @@ const {fizzBuzz} = require('./fizzBuzz')
 
 describe('fizzBuzz', () => {
     test('Should returns "Fizz" for multiples of 3', () => {
-        const result = fizzBuzz(3)
+        const number = 3
+        const fizz = "Fizz"
+        const result = fizzBuzz(number)
 
-        expect(result).toBe("Fizz")
+        expect(result).toBe(fizz)
     })
 
     test('Should returns "Buzz" for multiples of 5', () => {
-        const result = fizzBuzz(5)
+        const number = 5
+        const Buzz = "Buzz"
+        const result = fizzBuzz(number)
 
-        expect(result).toBe("Buzz")
+        expect(result).toBe(Buzz)
     })
 
     test('Should returns "FizzBuzz" for multiples of 3 & 5', () => {
-        const result = fizzBuzz(15)
+        const number = 15
+        const FizzBuzz = "FizzBuzz"
+        const result = fizzBuzz(number)
 
-        expect(result).toBe("FizzBuzz")
+        expect(result).toBe(FizzBuzz)
     })
 
     test('Should returns 1 when fizzBuzz is called with number = 1', () => {
-        const result = fizzBuzz(1)
+        const number = 1
+        const result = fizzBuzz(number)
 
-        expect(result).toBe(1)
+        expect(result).toBe(number)
     })
 })
