d82127b4c4acdebd81f2a40e6714d6ab270d01e8
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-15 15:30:34 +0200

    💄 Add interface to view function results

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -5,4 +5,17 @@ function fizzBuzz(number) {
     return number
 }
 
+//display FizzBuzz
+if (typeof document !== "undefined") {
+    const button = document.getElementById("valid-btn");
+    const display = document.getElementById("display");
+  
+    button?.addEventListener("click", () => {
+        console.log('hello :', "hello")
+      const num = document?.getElementById("number").value;
+      const result = fizzBuzz(Number(num));
+      display.innerHTML = `${result}`;
+    });
+  }
+
 module.exports = {fizzBuzz}
